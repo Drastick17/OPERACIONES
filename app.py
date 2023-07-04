b69825853c4d981ea082f31a8dc252dc56609f7b
DannySolano <danny@hex.ec>
2023-07-04 14:44:45 -0500

    adding sql login

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QPalette, QColor
+from sqlite import setConnection,validate_user
 
 app = QtWidgets.QApplication([])
+cursor = setConnection()
 
 GUI_LOGIN = uic.loadUi("ui/Login.ui")
 GUI_FORM = uic.loadUi("ui/Formulario.ui")
@@ -10,14 +13,34 @@ GUI_MATRIZ = uic.loadUi("ui/Matriz.ui")
 # GUI LOGIN
 GUI_LOGIN.show()
 
-# ACTIONS
+# STYLES
+def set_error_style(line_edit):
+    palette = line_edit.palette()
+    palette.setColor(QPalette.Base, QColor(255, 0, 0))  
+    line_edit.setPalette(palette)
 
+def set_text_error_style(label):
+    label.setStyleSheet("color: red; font-weight: bold;")  
+
+# ACTIONS
 def login():
     name = GUI_LOGIN.textbox_user.text()
     password = GUI_LOGIN.textbox_pass.text()
-    if(name == "Danny" and password == "1234"):
+    user = validate_user(cursor, name, password)
+    if(user):
         GUI_LOGIN.hide()
         GUI_FORM.show()
+    else:
+        GUI_LOGIN.error_user.setText('Usuario incorrecto')
+        GUI_LOGIN.error_password.setText('Contraseña incorrecta')
+        #SET STYLES
+        set_text_error_style(GUI_LOGIN.error_user)
+        set_text_error_style(GUI_LOGIN.error_password)
+        set_error_style(GUI_LOGIN.textbox_user)
+        set_error_style(GUI_LOGIN.textbox_pass)
+
+def register():
+    pass
 
 # ADD GUI ACTIONS
 GUI_LOGIN.btn_login.clicked.connect(login)
---
+++ sqlite.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+# CONNECTION
+
+try:
+    def setConnection():
+        connection = sqlite3.connect('sqlite.db')
+        cursor = connection.cursor()
+
+        # CREATE TABLE
+
+        users_table = '''
+            CREATE TABLE IF NOT EXISTS USERS (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                password TEXT NOT NULL
+            );
+        '''
+
+        # CREATE DEFAULT ADMINS
+        
+        users = [
+            ('Ariel', '123py'),
+            ('Danny', '123py'),
+            ('Robinson', '123py')
+        ]
+
+        users_query = "INSERT INTO USERS (name, password) VALUES (?, ?)"
+
+        cursor.execute(users_table)
+        cursor.executemany(users_query, users)
+        return cursor
+
+    # QUERYS
+
+    def validate_user(cursor, name, password):
+        query = 'SELECT * FROM USERS WHERE NAME = ? AND PASSWORD = ?'
+        cursor.execute(query, (name, password))
+        user = cursor.fetchone()
+        if user:
+            return True
+        return False
+
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
